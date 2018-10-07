66ee0c457a2ce2adb8f2147d99cbd673c4b39f13
河原 千佳 <Chika@kawara-chika-no-MacBook-Air.local>
2018-10-07 18:00:42 +0900

    update .gitignore

---
+++ wp-content/themes/stingerplus2-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) {
+exit;
+}
+//親スタイルシート読み込み
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
