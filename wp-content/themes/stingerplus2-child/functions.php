b10bc11e7832c607d0bf678fa5ad6093e15d4cd9
河原 千佳 <Chika@kawara-chika-no-MacBook-Air.local>
2018-10-08 18:31:03 +0900

    ユーティリティメニュー追加

--- wp-content/themes/stingerplus2-child/functions.php
+++ wp-content/themes/stingerplus2-child/functions.php
@@ -7,3 +7,15 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// メニューカスタム版
+register_nav_menus(
+ array(
+  'primary-menu' => __( 'ヘッダー用メニュー', 'default' ),
+  'secondary-menu' => __( 'フッター用メニュー', 'default' ),
+  'sidepage-menu' => __( 'サイド用メニュー', 'default' ),
+  'smartphone-menu' => __( 'スマートフォン用メニュー', 'default' ),
+  'utility_top'=> __( 'ユーティリティメニュー（上）', 'default' ),
+  'utility_bottom'=> __( 'ユーティリティメニュー（下）', 'default' )
+ )
+);
