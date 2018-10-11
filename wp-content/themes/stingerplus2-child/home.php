421516dad8aec276263b00ed1bdd38491ba9b293
Chika Kawara <shikinami@gmail.com>
2018-10-11 21:59:01 +0900

    「研修・セミナー」トップのお知らせ削除

--- wp-content/themes/stingerplus2-child/home.php
+++ wp-content/themes/stingerplus2-child/home.php
@@ -4,42 +4,6 @@
 	<div id="contentInner">
 		<main <?php st_text_copyck(); ?>>
 			<article>
-
-				<div class="home-post post">
-
-				<?php if (is_paged()): //2ページ目以降は表示しない ?>
-				<?php else: ?>
-
-						<?php if ( trim( $GLOBALS["stdata9"] ) !== '' ) { //固定記事挿入していない場合
-						}else{ ?>
-							<?php if( (isset($GLOBALS['stdata54'])) && ($GLOBALS['stdata54'] === 'yes') ): //トップ記事上部表示にチェックがある場合 ?>
-								<div class="nowhits <?php st_noheader_class(); ?>"><?php get_template_part( 'popular-thumbnail' ); //任意のエントリ ?></div>
-							<?php endif; ?>
-
-							<?php if ( is_front_page() && is_active_sidebar( 12 ) ) { ?>
-								<?php if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 12 ) ) : else : //トップ上部のウィジェット ?>
-								<?php endif; ?>
-							<?php } ?>
-							<?php get_template_part( 'news-st' ); //お知らせ ?>
-						<?php }?>
-
-						<?php get_template_part( 'st-topin' ); //固定記事挿入 ?>
-
-						<?php //任意のエントリ
-						if( ( is_home() || is_front_page() ) && ( isset($GLOBALS['stdata59']) && $GLOBALS['stdata59'] === 'yes' ) ){ //トップ記事の場合 ?>
-						<div style="padding-top:20px;">
-							<?php if ( isset($GLOBALS['stdata5']) && $GLOBALS['stdata5'] === 'yes' ) {
-								get_template_part( 'popular-thumbnail-off' );
-							}else{
-								get_template_part( 'popular-thumbnail-on' );
-							} ?>
-						</div>
-						<?php }  //任意のエントリここまで ?>
-
-				<?php endif; ?>
-
-				</div>
-
 				<aside>
 
 					<?php //記事一覧
