887204b9c28845dd356de2805341cbab5a3e6e23
Chika Kawara <shikinami@gmail.com>
2018-10-18 20:35:50 +0900

    functions.phpにカスタムメニュー追加

--- wp-content/themes/stinger8-child/functions.php
+++ wp-content/themes/stinger8-child/functions.php
@@ -6,3 +6,14 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// メニューカスタム版
+register_nav_menus(
+ array(
+  'primary-menu' => __( 'ヘッダー用メニュー', 'default' ),
+  'secondary-menu' => __( 'フッター用メニュー', 'default' ),
+  'smartphone-menu' => __( 'スマートフォン用メニュー', 'default' ),
+  'utility_top'=> __( 'ユーティリティメニュー（上）', 'default' ),
+  'utility_bottom'=> __( 'ユーティリティメニュー（下）', 'default' )
+ )
+);
