80cb05ab16b4c7ac6c2ae46cebfdf46963a34438
Chika Kawara <shikinami@gmail.com>
2018-11-01 22:19:59 +0900

    topカテゴリー別メッセージエリア仮組み

--- wp-content/themes/stinger8-child/front-page.php
+++ wp-content/themes/stinger8-child/front-page.php
@@ -2,10 +2,49 @@
 
 <div id="content" class="clearfix">
 	<div id="contentInner">
-		<div class="st-main">
+		<div class="st-main" style="margin-right:0;">
 			<article>
 
-					<?php echo 'front-page.php' ?>
+					<section class="for-people-box">
+						<div class="for-people-box--inner">
+							<a href="">
+								<dl>
+									<dt><span class="people-category">あああああ</span><br>の方へ</dt>
+									<dd>
+										<strong>いいいいいい</strong>
+										<p>うううううううううううううううううううううううううううううううううううううううううううううううう</p>
+									</dd>
+								</dl>
+							</a>
+							<a href="">
+								<dl>
+									<dt><span class="people-category">あああああ</span><br>の方へ</dt>
+									<dd>
+										<strong>いいいいいい</strong>
+										<p>うううううううううううううううううううううううううううううううううううううううううううううううう</p>
+									</dd>
+								</dl>
+							</a>
+							<a href="">
+								<dl>
+									<dt><span class="people-category">あああああ</span><br>の方へ</dt>
+									<dd>
+										<strong>いいいいいい</strong>
+										<p>うううううううううううううううううううううううううううううううううううううううううううううううう</p>
+									</dd>
+								</dl>
+							</a>
+							<a href="">
+								<dl>
+									<dt><span class="people-category">あああああ</span><br>の方へ</dt>
+									<dd>
+										<strong>いいいいいい</strong>
+										<p>うううううううううううううううううううううううううううううううううううううううううううううううう</p>
+									</dd>
+								</dl>
+							</a>
+						</div>
+					</section>
 
 			</article>
 		</div>
