154910d31dfa177a7b726773357f3675897d8bcb
Chika Kawara <shikinami@gmail.com>
2019-03-30 15:01:40 +0900

    絶対パスを相対パスに変換する関数追加

--- wp-content/themes/stinger8-child/functions.php
+++ wp-content/themes/stinger8-child/functions.php
@@ -90,3 +90,22 @@ register_nav_menus(
   'smartphone-submenu04' => __( 'スマートフォン用サブメニュー04', 'default' )
  )
 );
+
+// 絶対パスを相対パスに変換
+class relative_URI {
+    function relative_URI() {
+        add_action('get_header', array(&$this, 'get_header'), 1);
+        add_action('wp_footer', array(&$this, 'wp_footer'), 99999);
+    }
+    function replace_relative_URI($content) {
+        $home_url = trailingslashit(get_home_url('/'));
+        return str_replace($home_url, '/wordpress/', $content);
+    }
+    function get_header(){
+        ob_start(array(&$this, 'replace_relative_URI'));
+    }
+    function wp_footer(){
+        ob_end_flush();
+    }
+}
+new relative_URI();
